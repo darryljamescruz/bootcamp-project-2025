cf2c6f70b0d545b39108446ad3684e32cae80813
Darryl James Cruz <darryljamescruz@gmail.com>
2025-10-30 19:54:23 -0700

    feat: added some tailwind styling, maybe figure out shadcn next

--- bootcamp-milestone-2/components/navbar.tsx
+++ bootcamp-milestone-2/components/navbar.tsx
@@ -1,22 +1,32 @@
-import React from "react";
-import style from "./navbar.module.css";
 import Link from "next/link";
 
 export default function Navbar() {
   return (
-
-    // NOTE: we use "class" in HTML but React is quirky so we have to 
-    // change all the "class" to "className"
-    <header className={style.navbar}>
-      <h1> Darryl James Cruz&apos;s Personal Website </h1>
-      <nav>
-
-        <Link href="/">Home</Link>
-        <Link href="/blogs">Blogs</Link>
-        <Link href="/portfolio">Portfolio</Link>
-        <Link href="/resume">Resume</Link>
-        <Link href="/contact">Contact Me</Link>
-      </nav>
+    <header className="bg-gray-800 text-white shadow-md">
+      <div className="container mx-auto px-4 py-4">
+        <div className="flex flex-col md:flex-row md:items-center md:justify-between">
+          <h1 className="text-xl md:text-2xl font-bold mb-4 md:mb-0">
+            Darryl James Cruz&apos;s Personal Website
+          </h1>
+          <nav className="flex flex-col md:flex-row gap-4 md:gap-6">
+            <Link href="/" className="hover:text-blue-400 transition-colors duration-200">
+              Home
+            </Link>
+            <Link href="/blogs" className="hover:text-blue-400 transition-colors duration-200">
+              Blogs
+            </Link>
+            <Link href="/portfolio" className="hover:text-blue-400 transition-colors duration-200">
+              Portfolio
+            </Link>
+            <Link href="/resume" className="hover:text-blue-400 transition-colors duration-200">
+              Resume
+            </Link>
+            <Link href="/contact" className="hover:text-blue-400 transition-colors duration-200">
+              Contact Me
+            </Link>
+          </nav>
+        </div>
+      </div>
     </header>
   );
 }
